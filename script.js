241806f9af2bc00a829ceed4eb49612436cc19c7
Himanshu Singh <himanshusingh1k@gmail.com>
2025-10-28 11:21:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
 
-class Dog extends Animal {}
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
 
-class Cat extends Animal {}
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
